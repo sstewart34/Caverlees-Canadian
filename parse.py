4150be8490b7b0b2c0e09a3782888757f1ee4420
Brady Pacha <brady.pacha@tamu.edu>
2012-11-13 00:17:12 -0600

    revised parse.py file

--- parse.py
+++ parse.py
@@ -11,18 +11,26 @@ def stem(line):
 def main():
 	# Need to loop through each directory and each html doc
 	# Grab the topic name from the directory
+	topics = os.listdir('./DATA')
 	allPosts = []
-	json = {'topic':"", 'jobDescription':""}
-	file = open(sys.argv[1])
-	page = BeautifulSoup(file)
-	print page
-	jobDescription = page.findAll(id="jobBodyContent")
-	for descrip in jobDescription:
-		descrip = stem(str(descrip.get_text().rstrip()))
-		#print descrip.get_text()
-		json['jobDescription'] = descrip
-	allPosts.append(json)
-	file.close()	
-	print allPosts	
+	for topic in topics:
+		for entry in os.listdir('./DATA/' + topic):
+			path = './DATA/' + topic + '/' + entry
+			print path
+       			json = {'topic':"", 'jobDescription':""}
+			file = open(path)
+			page = BeautifulSoup(file)
+
+			jobDescription = page.findAll(id="jobBodyContent")
+			for descrip in jobDescription:
+      				description = stem(str(descrip.get_text().rstrip()))
+			        #print descrip.get_text()
+				json['jobDescription'] = description
+				json['topic'] = topic.replace('+',' ')
+		       	allPosts.append(json)
+			file.close()	
+	print allPosts
+
+
 if __name__ == "__main__":
 	main()
