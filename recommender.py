53578d29f8742390cbf53bc622e05a57d2643953
Nicholas Melnyk <ndmelnyk@nat-10-200-171-73.tamulink.tamu.edu>
2012-11-15 14:23:14 -0600

    Index by topics and index by languages

--- recommender.py
+++ recommender.py
@@ -10,10 +10,11 @@ def tokenize(tweet):
 class RecommenderAlgorithm(object):
     def __init__(self, docs):
 	self.jobs = docs
-	self.index = {}
+	self.indexByLanguage = {}
+	self.indexByTopic = {}
 	self.languages = []
 
-    def retrieve_docs(self):
+    def createLanguageList(self):	# Creates an index by languages
 	f = open('languages.txt','r')
 	for line in f.readlines():
 		self.languages.append((line.lower()).strip())
@@ -25,31 +26,56 @@ class RecommenderAlgorithm(object):
 			continue
 		text = job['jobDescription']
 		for word in tokenize(text.encode('utf8')):
-			if word in self.index:
-				if topic not in self.index[word]['topics']:
-					self.index[word]['topics'][topic] = 0
+			if word in self.indexByLanguage:
+				if topic not in self.indexByLanguage[word]['topics']:
+					self.indexByLanguage[word]['topics'][topic] = 0
 
-				self.index[word]['topics'][topic] += 1
-				self.index[word]['count'] += 1
+				self.indexByLanguage[word]['topics'][topic] += 1
+				self.indexByLanguage[word]['count'] += 1
 			else:
 				if word in self.languages:
-					self.index[word] = {'count':1, 'topics':{}}
-					self.index[word]['topics'][topic] = 1
+					self.indexByLanguage[word] = {'count':1, 'topics':{}}
+					self.indexByLanguage[word]['topics'][topic] = 1
 		count += 1
 	print count
-        sorted_list = [x for x in self.index.iteritems()]
+        sorted_list = [x for x in self.indexByLanguage.iteritems()]
 	sorted_list.sort(key=lambda x: x[1]['count']) # sort by count
 	sorted_list.reverse()
 	for item in sorted_list:
-		print item[0]
-	for item in sorted_list:
-		print  item[1]['count']#, item[1]['topics']
+		print item[0], item[1]['count'], item[1]['topics']
+	#for item in sorted_list:
+	#	print item[0]
+	#for item in sorted_list:
+	#	print  item[1]['count']#, item[1]['topics']
 	print len(sorted_list)
+	pass
+
+    def createTopicList(self):	# Creates an index by topics
+	for language in self.indexByLanguage:
+		for topic in self.indexByLanguage[language]['topics']:
+			if topic not in self.indexByTopic:
+				self.indexByTopic[topic] = {'languages':{}}	
+				self.indexByTopic[topic]['languages'][language] = self.indexByLanguage[language]['topics'][topic] 
+			else:
+				#if language not in self.indexByTopic[topic]['languages']:
+					#self.indexByTopic[topic]['languages'][language] = 0
+				self.indexByTopic[topic]['languages'][language] = self.indexByLanguage[language]['topics'][topic]
+	
+	sorted_list = [x for x in self.indexByTopic.iteritems()]
+	sorted_list.sort(key=lambda x: x[0]) # sort alphabetically by language
+	for item in sorted_list:
+		print item[0], item[1]
+	pass
+
+    def recommend(self, howMany):
+	pass
  
 def main():   
     docs = utils.read_docs()
     recommend = RecommenderAlgorithm(docs)
-    recommend.retrieve_docs()              
-                   
+    recommend.createLanguageList()              
+    recommend.createTopicList()
+    recommend.recommend(1)  
+                 
 if __name__=="__main__":
     main()
